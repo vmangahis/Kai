5e04244f6dc82b101d78fd978792390554aa3f9f
vmangahis <just.monika092299@gmail.com>
2022-11-20 02:41:01 +0800

    added cloudinary config

--- base/apps.py
+++ base/apps.py
@@ -1,8 +1,15 @@
+import cloudinary
 from django.apps import AppConfig
+from decouple import config
+
 
 
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
     def ready(self):
-        print('Server run!')
+        cloudinary.config(
+            cloud_name = config("CLOUDINARY_NAME"),
+            api_key = config("CLOUDINARY_API_KEY"),
+            api_secret = config("CLOUDINARY_SECRET_API")
+        )
