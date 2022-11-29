2deb621a9dcdf3539cefd994fa03ce9cc73711ce
vmangahis <just.monika092299@gmail.com>
2022-11-29 16:38:58 +0800

    syncing changes

--- base/static/js/catalog_info.js
+++
@@ -1,9 +0,0 @@
-let authenticated = JSON.parse(document.getElementById('authenticated').textContent);
-let testData = JSON.parse(document.getElementById('data'))
-
-if (!authenticated) {
-    document.getElementById("login-watchlist").addEventListener("click", (ev) => {
-        document.location.href = "/login/";
-    });
-}
-
